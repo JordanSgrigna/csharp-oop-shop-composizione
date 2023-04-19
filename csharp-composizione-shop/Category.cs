5f96df785487830ec2ca8d080f6692be2cb3dcb2
JordanSgrigna <kaliwyrdiscord@gmail.com>
2023-04-19 15:10:53 +0200

    product, shop, category classes

---
+++ csharp-composizione-shop/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_composizione_shop
+{
+	internal class Category
+	{
+	}
+}
