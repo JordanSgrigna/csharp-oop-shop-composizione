c272129fc96ed02c47629a56399cddd5bbc18526
JordanSgrigna <kaliwyrdiscord@gmail.com>
2023-04-19 16:12:15 +0200

    Category added to product

--- csharp-composizione-shop/Product.cs
+++ csharp-composizione-shop/Product.cs
@@ -6,7 +6,126 @@ using System.Threading.Tasks;
 
 namespace csharp_composizione_shop
 {
-	internal class Product
-	{
+	public class Product
+	{	
+		//ATTRIBUTES + PROPERTIES
+		private string code;
+		public string Code
+		{
+			get
+			{
+				return this.code;
+			}
+		}
+
+		private string name;
+		public string Name
+		{
+			get
+			{
+				return this.name;
+			}
+			set
+			{
+				this.name = value;
+			}
+		}
+
+		private string description;
+		public string Description
+		{
+			get
+			{
+				return this.description;
+			}
+			set
+			{
+				this.description = value;
+			}
+		}
+
+		private float price;
+		public float Price
+		{
+			get
+			{
+				return this.price;
+			}
+			set
+			{
+				this.price = value;
+			}
+		}
+
+		private int tax;
+		public int Tax
+		{
+			get
+			{
+				return this.tax;
+			}
+			set
+			{
+				this.tax = value;
+			}
+		}
+
+		//CONSTRUCTOR
+		public Product(string name, string description, float price, int tax = 22)
+		{
+			this.name = name;
+			this.description = description;
+			this.code = getCodePadLeft8();
+			this.price = price;
+			this.tax = tax;
+		}
+
+		//METHODS
+		private string getCodePadLeft8()
+		{
+			Random rnd = new Random();
+			int numeroCifre = rnd.Next(1, 9);
+
+			int codeInt = 0;
+
+			switch (numeroCifre)
+			{
+				case 1:
+					codeInt = rnd.Next(1, 10);
+					break;
+				case 2:
+					codeInt = rnd.Next(10, 100);
+					break;
+				case 3:
+					codeInt = rnd.Next(100, 1000);
+					break;
+				case 4:
+					codeInt = rnd.Next(1000, 10000);
+					break;
+				case 5:
+					codeInt = rnd.Next(10000, 1000000);
+					break;
+				case 6:
+					codeInt = rnd.Next(100000, 1000000);
+					break;
+				case 7:
+					codeInt = rnd.Next(1000000, 10000000);
+					break;
+				case 8:
+					codeInt = rnd.Next(10000000, 100000000);
+					break;
+
+			}
+
+			string codeString = codeInt.ToString();
+			string codeStringPadded = codeString.PadLeft(8, '0');
+			return codeStringPadded;
+
+		}
+
+
+
+
+
 	}
 }
--- csharp-composizione-shop/Shop.cs
+++ csharp-composizione-shop/Shop.cs
@@ -6,7 +6,60 @@ using System.Threading.Tasks;
 
 namespace csharp_composizione_shop
 {
-	internal class Shop
+	public class Shop
 	{
+		private string name;
+		public string Name
+		{
+			get
+			{
+				return this.name;
+			}
+			set 
+			{ 
+				this.name = value; 
+			}
+
+		}
+
+		private string city;
+		public string City
+		{
+			get
+			{
+				return this.city;
+			}
+		}
+
+		private string address;
+		public string Address
+		{
+			get
+			{
+				return this.address;
+			}
+		}
+
+		private string houseNumber;
+		public string HouseNumber
+		{
+			get
+			{
+				return this.houseNumber;
+			}
+		}
+
+		private List<Product> shopProducts;
+
+		//CONSTRUCTOR
+		public Shop(string name, string city, string address, string houseNumber)
+		{
+			this.name = name;
+			this.city = city;
+			this.address = address;
+			this.houseNumber = houseNumber;
+			this.shopProducts = new List<Product>();
+		}
+
 	}
 }
